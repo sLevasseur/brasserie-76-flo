48bdb80ef10f063e0353a2322c0ef9daec3f2ff9
kompotejam <simonphilippe.levasseur@gmail.com>
2021-06-09 08:55:40 +0200

    add custom domain to qovery yml

--- bakpak_project/bakpak_website/models.py
+++ bakpak_project/bakpak_website/models.py
@@ -14,11 +14,11 @@ class CoordinatesMapDB(models.Model):
         ('Bar', 'Bar'),
         ('Marche', 'Marché')
     )
-    url_coordinates = models.CharField(max_length=500, default="N/A")
+    url_coordinates = models.CharField(max_length=500, default="N/A", help_text="Copiez-collez une url de google maps, le plus important c'est ce qu'il y aprés !3d et !4d.")
     name_of_locations = models.CharField(max_length=300)
     adresse = models.CharField(max_length=300)
     code_postal = models.CharField(max_length=300)
     localite = models.CharField(max_length=300)
-    categories = models.CharField(max_length=300, choices=CHOICES, default="Brasserie")
-    informations_supplementaires = HTMLField()
+    categories = models.CharField(max_length=300, choices=CHOICES, default="Brasserie", help_text="Chaque catégorie renvoie à un logo différent sur la carte.")
+    informations_supplementaires = HTMLField(help_text="Ici tu peux mettres les horaires d'ouvertures, les prix de ventes etc...")
 
